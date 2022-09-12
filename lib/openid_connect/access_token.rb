64a3bd6fc0c0885a863e1b448d714e5279ca2d1e
Andrew <optimization@gmail.com>
2022-09-13 02:26:12 +0300

    Added sites requiring token as exceptions

--- lib/openid_connect/access_token.rb
+++ lib/openid_connect/access_token.rb
@@ -1,5 +1,7 @@
 module OpenIDConnect
   class AccessToken < Rack::OAuth2::AccessToken::Bearer
+    REQUIRE_TOKEN = ['tradewing.com']
+
     attr_required :client
     attr_optional :id_token
 
@@ -10,12 +12,17 @@ module OpenIDConnect
 
     def userinfo!(params = {})
       hash = resource_request do
-        get client.userinfo_uri, { "access_token" => @access_token }.merge(params)
+        params = { "access_token" => @access_token }.merge(params) if requires_token?
+        get client.userinfo_uri, params
       end
       ResponseObject::UserInfo.new hash
     end
 
     private
+
+    def requires_token?
+      REQUIRE_TOKEN.any? { |host| client.host.to_s.include?(host) }
+    end
 
     def resource_request
       res = yield
